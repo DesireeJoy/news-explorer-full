24bc4a7ac90bb5a8f8a50a8768acc13a578cceb4
DesireeJoy <desireebradish@gmail.com>
2021-10-21 18:19:27 -0400

    remove validator

--- news-explorer-api/models/user.js
+++ news-explorer-api/models/user.js
@@ -6,11 +6,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    validate: {
-      validator(v) {
-        return validator.isEmail(v);
-      },
-      message: 'Invalid email',
+    // validate: {
+    //   validator(v) {
+    //     return validator.isEmail(v);
+    //   },
+      // message: 'Invalid email',
     },
 
   },
