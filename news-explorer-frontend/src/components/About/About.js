29eddf3aef24fb165f2b4cfdab7c6048875d21a1
DesireeJoy <desireebradish@gmail.com>
2021-10-24 17:01:25 -0400

    Finish footer

--- news-explorer-frontend/src/components/About/About.js
+++ news-explorer-frontend/src/components/About/About.js
@@ -8,8 +8,7 @@ function About(props) {
                 <img className='about__image' src={creator} alt='the author' />
                 <div className='about__container'>
                     <h2 className='about__heading'>Desiree Joy</h2>
-                    <p className='about__description'>is a 2021 full stack bootcamp graduate of Practicum by Yandex. 
-                    <br /> She is proficient in the MERN stack and is eager to focus on accesibility on the web.</p>
+                    <p className='about__description'>Welcome to the final project of Desiree´Joy Bradish, Software Developer. This was created for the Practicum by Yandex Full Stack bootcamp. This project has a custom build API that is hosted on a localhost on a Netlify site. The front end is also hosted on the same site. A reverse proxy setup with Nginx and secured with Certbot controls the traffic allowing the front and back ends to communicate. This project also utilizes a news API service that users can query and then save those articles to their own account.</p>
                 </div>
             </section>
         )
--- news-explorer-frontend/src/components/Footer/Footer.js
+++ news-explorer-frontend/src/components/Footer/Footer.js
@@ -1,4 +1,5 @@
-import facebookLogo from '../../images/facebook.svg';
+import twitterLogo from '../../images/twitter.svg';
+import linkedInLogo from '../../images/linkedIn.svg';
 import githubLogo from '../../images/github.svg';
 function Footer(){
     return(
@@ -7,10 +8,10 @@ function Footer(){
                 <p className='footer__copyright'>© 2021 Supersite, Powered by News API</p>
                 <nav className='footer__link'>
                     <ul className='footer__list'>
-                        <li><a className='footer__link' href= '/' target='_self' rel='noreferrer'>Home</a></li>
-                        <li className='footer__list-item'><a className='footer__link' href= 'https://practicum.yandex.com/' target='_blank' rel='noreferrer'>Practicum by Yandex</a></li>
-                        <li><a className='footer__link'href='https://github.com/lmontem/news-explorer-frontend' target='_blank' rel='noreferrer'><img src={githubLogo} alt='github logo' /></a></li>
-                        <li><a className='footer__link'href='https://www.facebook.com/YPracticum' target='_blank' rel='noreferrer'><img src={facebookLogo} alt='facebook logo' /></a></li>
+                        <li className='footer__list-item'><a className='footer__link' href= '/' target='_self' rel='noreferrer'>Home</a></li>
+                        <li className='footer__icon'><a className='footer__link'href='https://github.com/DesireeJoy/' target='_blank' rel='noreferrer'><img src={githubLogo} className='footer__icon-image' alt='github logo' /></a>
+                        <a className='footer__link'href='https://www.linkedin.com/in/desiree-bradish-a5728bb/' target='_blank' rel='noreferrer'><img src={linkedInLogo} className='footer__icon-image' alt='linkedIn logo' /></a>
+                        <a className='footer__link'href='https://twitter.com/DesireeJoy' target='_blank' rel='noreferrer'><img src={twitterLogo} className='footer__icon-image' alt='twitter logo' /></a></li>
                         
                     </ul>
                 </nav>
