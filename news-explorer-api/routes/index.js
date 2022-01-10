3f3b9ba65d57a0496e05930a881d46c6b7f7b129
DesireeJoy <desireebradish@gmail.com>
2022-01-10 08:21:21 -0500

    Updated google Auth

--- news-explorer-api/routes/index.js
+++ news-explorer-api/routes/index.js
@@ -25,15 +25,15 @@ router.post(
 
 router.post(
   '/signup',
-  // celebrate({
-  //   body: Joi.object()
-  //     .keys({
-  //       email: Joi.string().required().email().messages({ 'any.required': '"Y" dont you see me' }),
-  //       password: Joi.string().required().min(8),
-  //       name: Joi.string().required().min(2).max(30),
-  //     })
-  //     .unknown(true),
-  // }),
+  celebrate({
+    body: Joi.object()
+      .keys({
+        email: Joi.string().required().email().messages({ 'any.required': '"Y" dont you see me' }),
+        password: Joi.string().required().min(8),
+        name: Joi.string().required().min(2).max(30),
+      })
+      .unknown(true),
+  }),
   createUser,
 );
 router.get(
